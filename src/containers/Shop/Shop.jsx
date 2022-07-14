bc7876af74b6ab2f7c76ac8308ae06c9d0e73ed7
geraldalex <alex1989chekan@gmail.com>
2022-07-14 12:29:09 +0300

    Menu

--- src/containers/Shop/Shop.jsx
+++ src/containers/Shop/Shop.jsx
@@ -1,12 +1,74 @@
 
+ 
 import styles from "./Shop.module.css";
-
-import { getApiResource, DB_ROOT } from '../../utils/network'; 
-
-
-
+ 
+import { getApiResource, DB_ROOT,PROXY } from '../../utils/network';
+import { useEffect, useState } from "react";
+ 
+ 
+ 
 const Shop = () => {
-  return <h1 className={styles.header}>Shop Hello</h1>;
+ 
+const [restorans, setRestorans] = useState(null)
+const [menuMc, setMenuMc] = useState(null)
+const [menuKFC, setMenuKFC] = useState(null)
+const [menuRis, setMenuRis] = useState(null)
+ 
+const getResource = async (url) => {
+const res  = await getApiResource(url);
+ 
+const restoranList = Object.keys(res.restorans);
+setRestorans(restoranList)
+
+
+const menuListKFC = res.restorans.KFC.map(({id, name, img, price}) => {
+  return {
+    id,
+    name,
+    img,
+    price
+  }
+ 
+})
+setMenuKFC(menuListKFC)
+}
+ 
+useEffect(() => {
+  getResource(PROXY + DB_ROOT)
+}, [])
+ 
+  return (
+    <>
+    
+ {restorans && (
+  <ul>
+    {
+      restorans.map(element => <li key={element}>{element}</li>)
+    }
+  </ul>
+ )
+  
+
+ }
+
+{menuKFC && (
+  <ul>
+    {
+      menuKFC.map(({id, name, img, price}) => <li key={id}>
+        {name}
+        <img src={img} />
+        {price}
+        </li>)
+    }
+  </ul>
+ )
+  
+
+ }
+    
+    </>
+  )
 };
 
+ 
 export default Shop;
--- src/utils/network.js
+++ src/utils/network.js
@@ -1,10 +1,19 @@
-export const DB_ROOT = 'https://my-json-server.typicode.com/geraldalex/DBDelivery'
+export const DB_ROOT = "https://my-json-server.typicode.com/geraldalex/DBDelivery/db";
+export const PROXY = "https://desolate-wildwood-43607.herokuapp.com/";
+ 
+export const getApiResource = async (url) => {
+  try {
+    const res = await fetch(url);
+ 
+    if (!res.ok) {
+      console.error("Could not fetch", res.status);
+      return false;
+    }
+    return await res.json();
+  } catch (error) {
+    console.error("Could not fetch", error.message);
+    return false;
+  }
+};
 
-export const getApiResource = async (url ) => {
-
-   fetch(url)
-    .then(response => response.json())
-    .then(json => console.log(json))
-
-}
 
